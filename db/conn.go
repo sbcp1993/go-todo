1a226495fe1d22f975be647bfca58f8763393695
sruthi.cp <sruthi.cp@consono.ai>
2021-02-11 10:16:15 +0530

    solve connection issue

--- db/conn.go
+++ db/conn.go
@@ -35,7 +35,7 @@ func NewDBConnection() (conn *DBConnection, err error) {
 	conn = new(DBConnection)
 	conn.dbType = "postgres"
 
-	conn.DB, err = sql.Open("postgres", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
+	conn.DB, err = sql.Open("postgres", "postgres://postgres:postgres@postgres:5432/postgres?sslmode=disable")
 	if err != nil {
 		return
 	}
